c6f7b10875e0ad97a1def0517f7ebc2c7dae6456
Alex Baron <526473+gb96@users.noreply.github.com>
2024-10-01 11:43:36 -0400

    20241001: hide confidential data

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 
-const BASE_URL = 'https://api.themoviedb.org/3/';
-const API_KEY_PARAM = "?api_key=d24a1350ea69817f21be54818f833b11";
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+const API_KEY_PARAM = `?api_key=${process.env.REACT_APP_API_KEY}`;
 const BACKDROP_URL = 'https://image.tmdb.org/t/p/original/';
 const SMALL_IMG_COVER_BASE_URL = "https://image.tmdb.org/t/p/w300/";
 
